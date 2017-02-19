af6971fa0ec4ad9ad7d80a2d66e72ce6daf6c971
masozow <marvingjo@hotmail.com>
2017-02-18 18:10:33 -0600

    Terminado el formulario de contacto

--- pagina/views.py
+++ pagina/views.py
@@ -1,21 +1,34 @@
+from django.conf import settings
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
 from django.core.serializers.json import DjangoJSONEncoder #para decofificar todos los datos de MySql
 from django.core.mail import send_mail #Falta configurar los settings
+import json
 
 def index(request):
     return render(request, 'pagina/index.html', {})
 
 
 def correo_electronico(request):
-    if request.method=='POST':
-        form_contacto=Form_Contacto(request.POST) #se obtiene el formulario que vino con el request
-        if form_contacto.is_valid(): #se comprueba que el formulario sea válido, es decir, que cumpla con las restricciones descritas en el model
-            datos_contacto=form_contacto.cleaned_data
+    if request.method == 'POST':
+        response_data = {}
+        try:
+            correo_cliente = request.POST.get('correo_electronico')
+            nombre_cliente = request.POST.get('nombre')
+            servicios_cliente = request.POST.get('servicios') #llamar por el nombre del objeto json que se envia como 'data' dentro de la consulta Ajax
             from_email=settings.EMAIL_HOST_USER
             to_list=[from_email]
-            send_mail(datos_contacto['asunto_contacto'],'De: '+datos_contacto['nombre_contacto']+' <'+datos_contacto['correo_contacto']+'>'+'\n\n' +datos_contacto['mensaje_contacto'],from_email,to_list,fail_silently=False)
-            #guardar=form_contacto.save()
-            form_contacto=Form_Contacto() #se obtiene un formulario limpio
+            send_mail('Solicitud de servicios','De: '+nombre_cliente+' <'+correo_cliente+'>'+'\n\n' +'Servicios: \n' +servicios_cliente,from_email,to_list,fail_silently=False)
+            response_data['Respuesta']='Gracias por tomarnos en cuenta.'+'\n\n'+'Pronto uno de nuestros miembros se pondra en contacto contigo ' +'\n'+'para conversar sobre los servicios en los que estás interesad@.'
+        except Exception as e:
+            response_data['Respuesta']='Ocurrió un error, por favor intenta contactarnos de nuevo.'+'\n'+str(e)
+        return HttpResponse(
+            json.dumps(response_data,cls=DjangoJSONEncoder),
+            content_type="application/json"
+        )
     else:
-        form_contacto=Form_Contacto() #se obtiene el formulario que vino con el request
-    return render(request, 'kadoshapp/WEBcontacto.html', {'form_contacto':form_contacto})
+        return HttpResponse(
+            json.dumps({"nothing to see": "this isn't happening"}),
+            content_type="application/json"
+        )
--- sitioweb/email_info.py
+++ sitioweb/email_info.py
@@ -2,5 +2,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS=True
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_HOST_USER='methrikssoftware@gmail.com'
-EMAIL_HOST_PASSWORD='M3thr!ks2017'
+EMAIL_HOST_PASSWORD='H0l@1234'
 EMAIL_PORT=587
